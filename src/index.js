256e823cbdc87be819f30ef66fbed88f11fe263f
Regis <boudinot.regis@yahoo.com>
2016-07-24 14:26:12 -0600

    formatting

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-    <Router history={browserHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
     </Route>
   </Router>,
