97bfc01631cd0fa7badac330411ca8d0ec956d2e
Regis <boudinot.regis@yahoo.com>
2016-07-24 14:58:48 -0600

    updating build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Router, Route, hashhistory } from 'react-router'
 import App from './App';
 
