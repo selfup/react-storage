92d0def771bce9879d7a7e0d666944b61551c122
Regis <boudinot.regis@yahoo.com>
2016-07-24 14:40:20 -0600

    see if this works on gh-pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route } from 'react-router'
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router>
     <Route path="/" component={App}>
     </Route>
   </Router>,
